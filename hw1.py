dfa9b418485730e9c1a19f7ffc9dd8d1b7f15e08
Piotr Aleksander Styczyński <piotrsty1@gmail.com>
2020-03-16 16:58:31 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -2,6 +2,7 @@ from typing import List
 
 import pandas as pd
 
+# Test
 CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
                       f"/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
 
